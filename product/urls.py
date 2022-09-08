0ff7a8dda589fd1b36f68b25a95f9173bb669167
Daniel Murphy <knysnacoder@gmail.com>
2022-09-08 16:11:23 +0200

    Adding the Product Detail view and url set

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from product import views
 urlpatterns = [
-  path('latest-products/', views.LatestProductsList.as_view(), name='latest-products')
+  path('latest-products/', views.LatestProductsList.as_view(), name='latest-products'),
+  path('products/<slug:category_slug>/<slug:product_slug>', views.ProductDetail.as_view()),
 ]
