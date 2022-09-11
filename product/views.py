42bea49a0b80a6d74844a16d79bd2ebc0aeff29c
Daniel Murphy <knysnacoder@gmail.com>
2022-09-11 11:44:58 +0200

    Adding a search viewset

--- product/views.py
+++ product/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.http import Http404
 from .models import Category, Product
 from .serializers import ProductSerializer, CategorySerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 class LatestProductsList(APIView):
@@ -37,3 +39,13 @@ class CategoryDetail(APIView):
     category = self.get_object(category_slug)
     serializer = CategorySerializer(category)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def search(request):
+  query = request.data.get('query', '')
+  if query:
+    products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+  else:
+    return Response({"products": []})
